3a481c5597d432a626844d6b22c62a670701b7d7
DanielMevs <dannymevs@hotmail.com>
2024-01-13 21:01:19 -0500

    started section about OOP

---
+++ DateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿var intnlPizzaDay23 = new DateTime(2023, 2, 9);
+
+Console.WriteLine($"Year is {intnlPizzaDay23.Year}");
+Console.WriteLine($"Month is {intnlPizzaDay23.Month}");
+Console.WriteLine($"Day is {intnlPizzaDay23.Day}");
+Console.WriteLine($"Day of the week is {intnlPizzaDay23.DayOfWeek}");
+
+var intnlPizzaDay24 = intnlPizzaDay23.AddYears(1);
+
+Console.WriteLine($"Year is {intnlPizzaDay24.Year}");
+Console.WriteLine($"Month is {intnlPizzaDay24.Month}");
+Console.WriteLine($"Day is {intnlPizzaDay24.Day}");
+Console.WriteLine($"Day of the week is {intnlPizzaDay24.DayOfWeek}");
+
+Console.ReadKey();
---
+++ Rectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿var rectangle1 = new Rectangle(5, 10);
+
+Console.WriteLine($"Width is {rectangle1.Width}");
+Console.WriteLine($"Height is {rectangle1.Height}");
+
+var rectangle2 = new Rectangle(2, 3);
+
+Console.WriteLine($"Width is {rectangle2.Width}");
+Console.WriteLine($"Height is {rectangle2.Height}");
+
+Console.ReadKey();
+
+class Rectangle
+{
+    public int Height;
+    public int Width;
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+
+    }
+}
