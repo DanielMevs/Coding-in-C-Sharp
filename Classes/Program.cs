323d91b4e0758855645e074dac5e808600e376ea
DanielMevs <dannymevs@hotmail.com>
2024-01-17 16:39:15 -0500

    completed field versus property section

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,55 +1,7 @@
-﻿var rectangle1 = new Rectangle(5, 10);
-var calculator = new ShapeMeasurementsCalculator();
-
-Console.WriteLine($"Width is {rectangle1.Width}");
-Console.WriteLine($"Height is {rectangle1.Height}");
-Console.WriteLine($"Area is {calculator.CalculateRectangleArea(rectangle1)}");
-Console.WriteLine($"Circumference is {calculator.CalculateRectangleCircumference(rectangle1)}");
-
-var rectangle2 = new Rectangle(2, 3);
-
-Console.WriteLine($"Width is {rectangle2.Width}");
-Console.WriteLine($"Height is {rectangle2.Height}");
-Console.WriteLine($"Area is {rectangle2.CalculateArea()}");
-Console.WriteLine($"Circumference is {rectangle2.CalculateCircumference()}");
-
-
-var appointmentTwoWeeksFromNow = new MedicalAppointment("Bob Smith", 14);
+﻿var appointmentTwoWeeksFromNow = new MedicalAppointment("Bob Smith", 14);
 var appointmentOneWeekFromNow = new MedicalAppointment("Margaret Smith");
 
 Console.ReadKey();
-
-class Rectangle
-{
-    public int Height;
-    public int Width;
-
-    public Rectangle(int width, int height)
-    {
-        Width = width;
-        Height = height;
-
-    }
-
-    public int CalculateCircumference() => 2 * Width + 2 * Height;
-
-
-    public int CalculateArea() => Width * Height;
-
-}
-
-class ShapeMeasurementsCalculator
-{
-    public int CalculateRectangleCircumference(Rectangle rectangle)
-    {
-        return 2 * rectangle.Width + 2 * rectangle.Height;
-    }
-    public int CalculateRectangleArea(Rectangle rectangle)
-    {
-        return rectangle.Width * rectangle.Height;
-    }
-}
-
 
 public class HotelBooking
 {
@@ -67,31 +19,6 @@ public class HotelBooking
 
 }
 
-
-
-
-
-public class Triangle
-{
-    private int _base;
-    private int _height;
-
-    public Triangle(int @base, int height)
-    {
-        _base = @base;
-        _height = height;
-    }
-
-    public int CalculateArea()
-    {
-        return (_base * _height) / 2;
-    }
-    public string AsString()
-    {
-        return $"Base is {_base}, height is {_height}";
-    }
-
-}
 
 
 class MedicalAppointmentPrinter
@@ -115,13 +42,13 @@ class MedicalAppointment
 
     public DateTime GetDate() => _date;
 
-    
+
     //public MedicalAppointment(string patientName) :
     //    this(patientName, 7)
     //{
     //}
 
-    public MedicalAppointment(string patientName="Unknown", int daysFromNow = 7)
+    public MedicalAppointment(string patientName = "Unknown", int daysFromNow = 7)
     {
         _patientName = patientName;
         _date = DateTime.Now.AddDays(daysFromNow);
@@ -150,6 +77,45 @@ class MedicalAppointment
             _date.Year,
             _date.Month + monthToAdd,
             _date.Day + daysToAdd);
+    }
+}
+
+
+public class Dog
+{
+    private string _name;
+    private string _breed;
+    private int _weight;
+
+    public Dog(string name, string breed, int weight)
+    {
+        _name = name;
+        _breed = breed;
+        _weight = weight;
+    }
+
+    public Dog(string name, int weight) : this(name, "mixed-breed", weight)
+    {
+    }
+
+    public string Describe()
+    {
+
+        return $"This dog is named {_name}, it's a {_breed}," +
+        $" and it weighs {_weight} kilograms, so it's a {DescribeSize()} dog.";
+    }
+    private string DescribeSize()
+    {
+        if (_weight < 5)
+        {
+            return "tiny";
+        }
+        if (_weight < 30)
+        {
+            return "medium";
+        }
+        return "large";
+
     }
 }
 
---
+++ Shapes/Program.cs
@@ -0,0 +1,110 @@
+﻿var rectangle1 = new Rectangle(5, 10);
+var calculator = new ShapeMeasurementsCalculator();
+
+Console.WriteLine($"Width is {rectangle1.Width}");
+rectangle1.Width = 15;
+Console.WriteLine($"Height is {rectangle1.GetHeight()}");
+Console.WriteLine($"Area is {calculator.CalculateRectangleArea(rectangle1)}");
+Console.WriteLine($"Circumference is {calculator.CalculateRectangleCircumference(rectangle1)}");
+
+var rectangle2 = new Rectangle(2, 3);
+
+Console.WriteLine($"Width is {rectangle2.Width}");
+Console.WriteLine($"Height is {rectangle1.GetHeight()}");
+Console.WriteLine($"Area is {rectangle2.CalculateArea()}");
+Console.WriteLine($"Circumference is {rectangle2.CalculateCircumference()}");
+
+
+Console.ReadKey();
+class Rectangle
+{
+    //const int NumberOfSides = 4;
+    //readonly int NumberOfSidesReadonly;
+
+
+    public Rectangle(int width, int height)
+    {
+        //NumberOfSidesReadonly = 4;
+        Width = GetLengthOrDefault(width, nameof(Width));
+        _height = GetLengthOrDefault(height, nameof(_height));
+
+
+
+    }
+
+    private int _width;
+
+    public int Width
+    {
+        get => _width;
+        set => _width = value;
+    }
+    
+
+    private int _height;
+
+    public int GetHeight() => _height;
+
+    public void SetHeight(int value)
+    {
+
+        if (value > 0)
+        {
+            _height = value;
+        }
+
+    }
+
+    private int GetLengthOrDefault(int length, string name)
+    {
+        const int defaultValueIfNonPositive = 1;
+        if (length <= 0)
+        {
+            Console.WriteLine($"{name} must be a positive number.");
+            return defaultValueIfNonPositive;
+        }
+        return length;
+    }
+
+    public int CalculateCircumference() => 2 * _width + 2 * GetHeight();
+
+
+    public int CalculateArea() => _width * GetHeight();
+
+}
+
+class ShapeMeasurementsCalculator
+{
+    public int CalculateRectangleCircumference(Rectangle rectangle)
+    {
+        return 2 * rectangle.Width + 2 * rectangle.GetHeight();
+    }
+    public int CalculateRectangleArea(Rectangle rectangle)
+    {
+        return rectangle.Width * rectangle.GetHeight();
+    }
+}
+
+
+public class Triangle
+{
+    private int _base;
+    private int _height;
+
+    public Triangle(int @base, int height)
+    {
+        _base = @base;
+        _height = height;
+    }
+
+    public int CalculateArea()
+    {
+        return (_base * _height) / 2;
+    }
+    public string AsString()
+    {
+        return $"Base is {_base}, height is {_height}";
+    }
+
+}
+
