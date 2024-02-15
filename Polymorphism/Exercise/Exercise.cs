a3ff113c31182991168fa247f5c4433dd195384d
DanielMevs <dannymevs@hotmail.com>
2024-02-15 18:10:10 -0500

    completed polymorphic shapes exercise

--- Polymorphism/Exercise/Exercise.cs
+++ Polymorphism/Exercise/Exercise.cs
@@ -110,3 +110,63 @@ public static class NumericTypesDescriber
 
     }
 }
+
+public static class ExerciseShapes
+{
+
+    public static List<double> GetShapesAreas(List<Shape> shapes)
+    {
+        var result = new List<double>();
+
+        foreach (var shape in shapes)
+        {
+            result.Add(shape.CalculateArea());
+        }
+
+        return result;
+    }
+}
+
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Square : Shape
+{
+    public double Side { get; }
+
+    public Square(double side)
+    {
+        Side = side;
+    }
+
+    public override double CalculateArea() => Side * Side;
+}
+
+
+public class Rectangle : Shape
+{
+    public double Width { get; }
+    public double Height { get; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double CalculateArea() => Height * Width;
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea() => Math.PI * (Radius * Radius);
+}
