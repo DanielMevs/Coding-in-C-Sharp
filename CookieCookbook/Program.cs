f2ab22af36ebbe59aebac9fbb6510953d61a0ca3
DanielMevs <dannymevs@hotmail.com>
2024-03-05 15:50:52 -0500

    completed section to print recipes to the console

--- CookieCookbook/Program.cs
+++ CookieCookbook/Program.cs
@@ -1,7 +1,11 @@
-﻿var cookiesRecipesApp = new CookiesRecipesApp(
+﻿using CookieCookbook.Recipes;
+using CookieCookbook.Recipes.Ingredients;
+using System.Diagnostics.Metrics;
+
+var cookiesRecipesApp = new CookiesRecipesApp(
     new RecipesRepository(),
     new RecipesConsoleUserInteraction());
-cookiesRecipesApp.Run();
+cookiesRecipesApp.Run("recipes.txt");
 
 public class CookiesRecipesApp
 {
@@ -15,30 +19,30 @@ public class CookiesRecipesApp
         _recipesRepository = recipesRepository;
         _recipesUserInteraction = recipesUserInteraction;
     }
-    public void Run()
+    public void Run(string filePath)
     {
         var allRecipes = _recipesRepository.Read(filePath);
         _recipesUserInteraction.PrintExistingRecipes(allRecipes);
 
-        _recipesUserInteraction.PromptToCreateRecipe();
+        //_recipesUserInteraction.PromptToCreateRecipe();
 
-        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+        //var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
 
-        if (ingredients.Count > 0)
-        {
-            var recipes = new Recipe(ingredients);
-            allRecipes.Add(recipe);
-            _recipesRepository.Write(filePath, allRecipes);
+        //if (ingredients.Count > 0)
+        //{
+        //    var recipes = new Recipe(ingredients);
+        //    allRecipes.Add(recipe);
+        //    _recipesRepository.Write(filePath, allRecipes);
 
-            _recipesUserInteraction.ShowMessage("Recipe added:");
-            _recipesUserInteraction.ShowMessage(recipe.ToString());
-        }
-        else
-        {
-            _recipesUserInteraction.ShowMessage(
-                "No ingredients have been selected. " +
-                "Recipe will not be saved.");
-        }
+        //    _recipesUserInteraction.ShowMessage("Recipe added:");
+        //    _recipesUserInteraction.ShowMessage(recipe.ToString());
+        //}
+        //else
+        //{
+        //    _recipesUserInteraction.ShowMessage(
+        //        "No ingredients have been selected. " +
+        //        "Recipe will not be saved.");
+        //}
 
         _recipesUserInteraction.Exit();
     }
@@ -48,6 +52,7 @@ public interface IRecipesUserInteraction
 {
     void ShowMessage(string message);
     void Exit();
+    void PrintExistingRecipes(IEnumerable<Recipe> allRecipes);
 }
 
 public class RecipesConsoleUserInteraction : IRecipesUserInteraction
@@ -62,13 +67,47 @@ public class RecipesConsoleUserInteraction : IRecipesUserInteraction
         Console.ReadLine();
     }
 
+    public void PrintExistingRecipes(IEnumerable<Recipe> allRecipes)
+    {
+        if(allRecipes.Count() > 0)
+        {
+            Console.WriteLine("Existing recipes are:" + Environment.NewLine);
+
+            var recipeCounter = 1;
+            foreach(var recipe in allRecipes)
+            {
+                Console.WriteLine($"*****{recipeCounter}*****");
+                Console.WriteLine(recipe);
+                Console.WriteLine();
+                ++recipeCounter;
+            }
+        }
+    }
 }
 
 public interface IRecipesRepository
 {
+    List<Recipe> Read(object filePath);
 }
 
 public class RecipesRepository : IRecipesRepository
 {
-
+    public List<Recipe> Read(object filePath)
+    {
+        return new List<Recipe>
+        {
+            new Recipe(new List<Ingredient>
+            {
+                new WheatFlour(),
+                new Butter(),
+                new Sugar()
+            }),
+            new Recipe(new List<Ingredient>
+            {
+                new CocoaPowder(),
+                new SpeltFlour(),
+                new Cinnamon()
+            })
+        };
+    }
 }
--- CookieCookbook/Recipes/Recipe.cs
+++ CookieCookbook/Recipes/Recipe.cs
@@ -10,5 +10,16 @@ namespace CookieCookbook.Recipes
         {
             Ingredients = ingredients;
         }
+
+        public override string ToString()
+        {
+            var steps = new List<string>();
+
+            foreach(var ingredient in Ingredients)
+            {
+                steps.Add($"{ingredient.Name}. {ingredient.PreparationInstructions}");
+            }
+            return string.Join(Environment.NewLine, steps);
+        }
     }
 }
