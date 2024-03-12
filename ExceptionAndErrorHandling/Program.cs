ded00634db6b5bb5f161101cc27312fc2ecd12d3
DanielMevs <dannymevs@hotmail.com>
2024-03-12 13:47:11 -0400

    completed exception handling section

---
+++ ExceptionAndErrorHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter a Number:");
+string input = Console.ReadLine();
+try
+{
+    var number = ParseStringToInt(input);
+    Console.WriteLine(
+        "String successfully parsed, the result is " + number);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(
+        $"An exception was thrown. Exception message: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Finally block is being excuted.");
+}
+
+
+Console.ReadKey();
+
+int ParseStringToInt(string input)
+{
+    return int.Parse(input);
+  
+}
