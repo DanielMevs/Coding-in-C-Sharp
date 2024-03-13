7d10e1db58073e9ef578c1eeb67ea62d48941f3e
DanielMevs <dannymevs@hotmail.com>
2024-03-13 12:15:47 -0400

    Completed try catch exercise that tries to divide two numbers, a and b, by each other. If b is zero, it will print so to the console and return 0

---
+++ ExceptionAndErrorHandling/Exercises/ExceptionsDivisionExercise.cs
@@ -0,0 +1,23 @@
+﻿namespace ExceptionAndErrorHandling.Exercises
+{
+    public class ExceptionsDivisionExercise
+    {
+        public static int DivideNumbers(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch
+            {
+                Console.WriteLine("Division by zero.");
+                return 0;
+
+            }
+            finally
+            {
+                Console.WriteLine("The DivideNumbers method ends.");
+            }
+        }
+    }
+}
