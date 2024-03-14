68ad747b06efda7b678ed02a8d2e9e4dc6984dab
DanielMevs <dannymevs@hotmail.com>
2024-03-14 16:01:06 -0400

    Defined multiple catch blocks for specific exception types for a single try block

--- ExceptionAndErrorHandling/Program.cs
+++ ExceptionAndErrorHandling/Program.cs
@@ -2,14 +2,25 @@
 string input = Console.ReadLine();
 try
 {
-    var number = ParseStringToInt(input);
-    Console.WriteLine(
-        "String successfully parsed, the result is " + number);
+    int number = ParseStringToInt(input);
+    var result = 10 / number;
+    Console.WriteLine($"10 / {number} is " + result);
 }
-catch(Exception ex)
+catch(FormatException ex)
 {
     Console.WriteLine(
-        $"An exception was thrown. Exception message: {ex.Message}");
+        "Wrong format. Input string is not parsable to int." +
+        $"Exception message: {ex.Message}");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine("Division by zero is an invalid operation. " +
+        "Exception message: " + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Unexpected error occured. " +
+        "Exception message: " + ex.Message);
 }
 finally
 {
