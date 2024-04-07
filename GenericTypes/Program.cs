83586d91df3128bde0357faa54974da27497e8b9
DanielMevs <dannymevs@hotmail.com>
2024-04-07 14:33:24 -0400

    continued type constraints section - the contraint on the implemented interface

--- GenericTypes/Program.cs
+++ GenericTypes/Program.cs
@@ -97,12 +97,32 @@ var employees = new List<Employee>
 var validPeople = GetOnlyValid(people);
 var validEmployees = GetOnlyValid(employees);
 
+
 foreach(var employee in validEmployees)
 {
     employee.GoToWork();
+
 }
+var john = new Person { Name = "John", YearOfBirth = 1980 };
+var anna = new Person { Name = "Anna", YearOfBirth = 1915 };
+
+PrintInOrder(10, 5);
+PrintInOrder("aaa", "bbb");
+//PrintInOrder(anna, john);
 
 Console.ReadKey();
+
+void PrintInOrder<T>(T first, T second) where T: IComparable<T>
+{
+    if(first.CompareTo(second) > 0)
+    {
+        Console.WriteLine($"{second} {first}");
+    }
+    else
+    {
+        Console.WriteLine($"{second} {first}");
+    }
+}
 
 IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> persons)
     where TPerson : Person
@@ -133,23 +153,25 @@ IEnumerable<T> CreateCollectionOfRandomLength<T>(int maxLength) where T : new()
     
     return result;
 }
-public class Person : IComparable<Person>
+public class Person //: IComparable<Person>
 {
     public string Name { get; init; }
     public int YearOfBirth { get; init; }
 
-    public int CompareTo(Person other)
-    {
-        if(YearOfBirth < other.YearOfBirth)
-        {
-            return 1;
-        }
-        else if (YearOfBirth > other.YearOfBirth)
-        {
-            return -1;
-        }
-        return 0;
-    }
+    public override string ToString() => $"{Name} bor in {YearOfBirth}";
+
+    //public int CompareTo(Person other)
+    //{
+    //    if(YearOfBirth < other.YearOfBirth)
+    //    {
+    //        return 1;
+    //    }
+    //    else if (YearOfBirth > other.YearOfBirth)
+    //    {
+    //        return -1;
+    //    }
+    //    return 0;
+    //}
 }
 public class Employee : Person
 {
