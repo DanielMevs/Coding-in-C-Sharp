be4f9ba711229625863b1ea164fb347da2770f98
DanielMevs <dannymevs@hotmail.com>
2024-04-08 20:15:38 -0400

    completed type constraints and IComparable - sorted list of full names exercise

---
+++ GenericTypes/Exercises/SortedListOfFullNames.cs
@@ -0,0 +1,32 @@
+﻿namespace Generics.Exercises
+{
+    public class SortedList<T> where T : IComparable<T>
+    {
+        public IEnumerable<T> Items { get; }
+
+        public SortedList(IEnumerable<T> items)
+        {
+            var asList = items.ToList();
+            asList.Sort();
+            Items = asList;
+        }
+    }
+
+    public class FullName : IComparable<FullName>
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        public int CompareTo(FullName other)
+        {
+            var lastNameComparisonResult = LastName.CompareTo(other.LastName);
+            if (lastNameComparisonResult != 0)
+            {
+                return lastNameComparisonResult;
+            }
+            else return FirstName.CompareTo(other.FirstName);
+        }
+    }
+}
