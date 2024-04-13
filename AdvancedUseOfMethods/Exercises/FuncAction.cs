6606c4c24f72e2ef78892bc0853d34673b30cd59
DanielMevs <dannymevs@hotmail.com>
2024-04-12 21:56:39 -0400

    completed action and func exercise

---
+++ AdvancedUseOfMethods/Exercises/FuncAction.cs
@@ -0,0 +1,19 @@
+﻿namespace AdvancedUseOfMethods.Exercises
+{
+    public class FuncAction
+    {
+        public void TestMethod()
+        {
+            Func<int, bool, double> Method1;
+
+            Func<DateTime> Method2;
+
+            Action<string, string> Method3;
+
+        }
+
+        public double Method1(int a, bool b) => 0;
+        public DateTime Method2() => default(DateTime);
+        public void Method3(string a, string b) { }
+    }
+}
