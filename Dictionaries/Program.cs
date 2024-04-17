e169b13bb0949811d36ee970aacda82cd44ace0c
DanielMevs <dannymevs@hotmail.com>
2024-04-16 21:09:39 -0400

    Implemented an algorithm to group employees by department and return a Dictionary where the key is the deparment and the value is the average salary per deparment

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -29,10 +29,70 @@ countryToCurrencyMapping.Add("Italy", "EUR");
 
 countryToCurrencyMapping["Poland"] = "PLN";
 
-foreach (var country in countryToCurrencyMapping)
+//foreach (var country in countryToCurrencyMapping)
+//{
+//    Console.WriteLine(
+//        $"Country: {country.Key}, "+
+//        $"currency: {country.Value}");
+//}
+
+var employees = new List<Employee>
 {
-    Console.WriteLine(
-        $"Country: {country.Key}, "+
-        $"currency: {country.Value}");
-}
+    new Employee("Jake Smith", "Space Navigation", 25000),
+    new Employee("Anna Blake", "Space Navigation", 29000),
+    new Employee("Barbara Oak", "Xenobiology", 21500),
+    new Employee("Damien Parker", "Xenobiology", 22000),
+    new Employee("Nisha Patel", "Mechanics", 21000),
+    new Employee("Gustavo Sanchez", "Mechanics", 20000),
+};
+
+var result = CalculateAverageSalaryPerDepartment(employees);
+
+
+
 Console.ReadKey();
+Dictionary<string, decimal> CalculateAverageSalaryPerDepartment(
+    IEnumerable<Employee> employees)
+{
+    var employeesPerDepartments = new Dictionary<string, List<Employee>>();
+
+    foreach(var employee in employees)
+    {
+        if (!employeesPerDepartments.ContainsKey(employee.Department))
+        {
+            employeesPerDepartments[employee.Department] = new List<Employee>();
+        }
+        employeesPerDepartments[employee.Department].Add(employee);
+    }
+
+    var result = new Dictionary<string, decimal>();
+
+    foreach(var employeesPerDept in employeesPerDepartments)
+    {
+        decimal sumOfSalaries = 0;
+
+        foreach(var employee in employeesPerDept.Value)
+        {
+            sumOfSalaries += employee.MonthlySalary;
+        }
+
+        var average = sumOfSalaries / employeesPerDept.Value.Count;
+
+        result[employeesPerDept.Key] = average;
+    }
+
+    return result;
+}
+public class Employee
+{
+    public Employee(string name, string department, decimal monthlySalary)
+    {
+        Name = name;
+        Department = department;
+        MonthlySalary = monthlySalary;
+    }
+
+    public string Name { get; init; }
+    public string Department { get; init; }
+    public decimal MonthlySalary { get; init; }
+}
