ada2555a9c3699981885037076cd3d5979151a50
DanielMevs <dannymevs@hotmail.com>
2024-04-22 21:13:54 -0400

    started custom cache project

---
+++ CustomCache/Program.cs
@@ -0,0 +1,47 @@
+﻿IDataDownloader dataDownloader = new SlowDataDownloader();
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.ReadKey();
+
+public class Cache<TKey, TData>
+{
+    private readonly Dictionary<TKey, TData> _cachedData = new();
+
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+    {
+        if (!_cachedData.ContainsKey(key))
+        {
+            _cachedData[key] = getForTheFirstTime(key);
+        }
+        return _cachedData[key];
+    }
+}
+
+public interface IDataDownloader
+{
+    string DownloadData(string resourceId);
+}
+
+public class SlowDataDownloader : IDataDownloader
+{
+    private readonly Cache<string, string> _cache = new();
+    public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, DownloadDataWithoutCaching);
+    }
+    private string DownloadDataWithoutCaching(string resourceId)
+    {
+        //let's imagine this method downloads real data,
+        //and it does it slowly
+        Thread.Sleep(1000);
+        return $"Some data for {resourceId}";
+    }
+}
+
