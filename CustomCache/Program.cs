b8a29e1323bdc922081a32f7e474dbeb4ce324ae
DanielMevs <dannymevs@hotmail.com>
2024-04-24 20:00:24 -0400

    composed many decorators together

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,6 +1,7 @@
 ﻿IDataDownloader dataDownloader = 
     new CachingDataDownloader(
-        new SlowDataDownloader());
+        new PrintingDataDownloader(
+            new SlowDataDownloader()));
 
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -26,6 +27,7 @@ public class Cache<TKey, TData>
     }
 }
 
+
 public class CachingDataDownloader : IDataDownloader
 {
     private readonly IDataDownloader _dataDownloader;
@@ -39,6 +41,23 @@ public class CachingDataDownloader : IDataDownloader
     public string DownloadData(string resourceId)
     {
         return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+
+public class PrintingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+
+    public PrintingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        var data = _dataDownloader.DownloadData(resourceId);
+        Console.WriteLine("Data is ready!");
+        return data;
     }
 }
 
