ca54a9e0c600f14ff081802227784fae5abb2b20
DanielMevs <dannymevs@hotmail.com>
2024-04-25 20:01:42 -0400

    started LINQ section

---
+++ Linq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var wordsNoUpperCase = new string[]
+        {
+            "quick", "brown", "fox"
+        };
+        Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));
+        var wordsWithUpperCase = new string[]
+        {
+            "quick", "brown", "FOX"
+        };
+        Console.WriteLine(IsAnyWordUpperCase(wordsWithUpperCase));
+        Console.ReadKey();
+    }
+    public static bool IsAnyWordUpperCase(
+        IEnumerable<string> words)
+    {
+        foreach (var word in words)
+        {
+            bool areAllUpperCase = true;
+            foreach(var letter in word)
+            {
+                if (char.IsLower(letter))
+                {
+                    areAllUpperCase = false;
+                }
+            }
+            if(areAllUpperCase)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
