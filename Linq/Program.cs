6558b0bfa498c140d63ad888b5d9321b0bdc9107
DanielMevs <dannymevs@hotmail.com>
2024-04-28 16:45:42 -0400

    explored deferred execution

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -15,23 +15,75 @@ namespace LinqTutorial
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 5, 3, 7, 1, 2, 4 };
-            var numbersWith10 = numbers.Append(10);
+            var words = new List<string> { "a",  "bb", "ccc", "dddd" };
 
-            //Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+            var shortWords = words.Where(word => word.Length < 3).ToList();
+
+            Console.WriteLine("First iteration");
+            foreach (var word in shortWords)
+            {
+                Console.WriteLine(word);
+            }
+            words.Add("e");
+
+            Console.WriteLine("Second iteration");
+            foreach (var word in shortWords)
+            {
+                Console.WriteLine(word);
+            }
+            var people = new List<Person>
+            {
+                new Person("John", "USA"),
+                new Person("Betty", "UK")
+            };
+            var allAmericans = people.Where(
+                person => person.Country == "USA");
+
+            var notAllAmericans = allAmericans.Take(100);
+
+            var animals = new List<string>()
+            {
+                "Duck", "Lion", "Dolphin", "Tiger"
+            };
+            var animalsWithD = animals.Where(
+                animal =>
+                {
+                    Console.WriteLine("Checking animals: " + animal);
+                    return animal.StartsWith('D');
+                });
+            foreach(var animal in animalsWithD)
+            {
+                Console.WriteLine(animal);
+            }
+            //var numbers = new List<int> { 5, 3, 7, 1, 2, 4 };
+            //var numbersWith10 = numbers.Append(10);
+
+            ////Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+            ////Console.WriteLine(
+            ////    "Numbers with 10: " + string.Join(", ", numbersWith10));
+
+            ////var oddNumbers = numbers.Where(number => number % 2 == 1);
+            //var orderedOddNumbers = numbers
+            //    .Where(number => number % 2 == 1)
+            //    .OrderBy(number => number);
+
             //Console.WriteLine(
-            //    "Numbers with 10: " + string.Join(", ", numbersWith10));
-
-            //var oddNumbers = numbers.Where(number => number % 2 == 1);
-            var orderedOddNumbers = numbers
-                .Where(number => number % 2 == 1)
-                .OrderBy(number => number);
-
-            Console.WriteLine(
-                "orderedOddNumbers: " + string.Join(", ", orderedOddNumbers));
+            //    "orderedOddNumbers: " + string.Join(", ", orderedOddNumbers));
 
             Console.ReadKey();
         }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public Person(string name, string country)
+        {
+            Name = name;
+            Country = country;
+        }
+
     }
     //class LinqExample
     //{
