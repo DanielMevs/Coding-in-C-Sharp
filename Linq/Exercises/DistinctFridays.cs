4cbeb957da0ca861dd3250688bed9d7b8140d6a4
DanielMevs <dannymevs@hotmail.com>
2024-05-06 20:01:20 -0400

    Completed distinct Fridays exercise

---
+++ Linq/Exercises/DistinctFridays.cs
@@ -0,0 +1,13 @@
+﻿namespace LinqTutorial.Exercises
+{
+    public class DistinctFridays
+    {
+        public static IEnumerable<DateTime> GetFridaysOfYear(int year, IEnumerable<DateTime> dates)
+        {
+            return dates.Where(date =>
+                date.Year == year &&
+                date.DayOfWeek == DayOfWeek.Friday)
+                .Distinct();
+        }
+    }
+}
