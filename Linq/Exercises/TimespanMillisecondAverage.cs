84242f34d2371927f9c6f890f9d49299cbefe506
DanielMevs <dannymevs@hotmail.com>
2024-05-09 20:04:16 -0400

    Completed calculate average duration in milliseconds exercise

---
+++ Linq/Exercises/TimespanMillisecondAverage.cs
@@ -0,0 +1,17 @@
+﻿namespace LinqTutorial.Exercises
+{
+    public class TimespanMillisecondAverage
+    {
+        public static double CalculateAverageDurationInMilliseconds(IEnumerable<TimeSpan> timeSpans)
+        {
+            if (timeSpans.Count() == 0)
+            {
+                throw new Exception("Empty list.");
+            }
+
+            return timeSpans.Select(timeSpan =>
+                timeSpan.Milliseconds)
+                .Average();
+        }
+    }
+}
