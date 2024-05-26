5377f741bfe399bfd6e8e4dd7883921fc21d789d
DanielMevs <dannymevs@hotmail.com>
2024-05-26 15:22:05 -0400

    improved csv processing by reducing the size of dictionaries

--- DotNetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
+++ DotNetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using CsvDataAccess.CsvReading;
 using CsvDataAccess.Interface;
+using CsvDataAccess.OldSolution;
 
 namespace CsvDataAccess.NewSolution;
 
@@ -7,6 +8,90 @@ public class FastTableDataBuilder : ITableDataBuilder
 {
     public ITableData Build(CsvData csvData)
     {
-        throw new NotImplementedException();
+        var resultRows = new List<FastRow>();
+
+        foreach (var row in csvData.Rows)
+        {
+            var newRowData = new Dictionary<string, object>();
+
+            for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
+            {
+                var column = csvData.Columns[columnIndex];
+                string valueAsString = row[columnIndex];
+                object value = ConvertValueToTargetType(valueAsString);
+
+                if (value is not null)
+                {
+                    newRowData[column] = value;
+                }
+            }
+
+            resultRows.Add(new FastRow(newRowData));
+        }
+
+        return new FastTableData(csvData.Columns, resultRows);
+    }
+
+    private object ConvertValueToTargetType(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return null;
+        }
+        if (value == "TRUE")
+        {
+            return true;
+        }
+        if (value == "FALSE")
+        {
+            return false;
+        }
+        if (value.Contains(".") && decimal.TryParse(value, out var valueAsDecimal))
+        {
+            return valueAsDecimal;
+        }
+        if (int.TryParse(value, out var valueAsInt))
+        {
+            return valueAsInt;
+        }
+        return value;
+    }
+}
+
+public class FastRow
+{
+    private Dictionary<string, object> _data;
+
+    public FastRow(Dictionary<string, object> data)
+    {
+        _data = data;
+    }
+
+    public object GetAtColumn(string columnName)
+    {
+        if (_data.ContainsKey(columnName))
+        {
+            return _data[columnName];
+        }
+
+        return null;
+    }
+}
+
+public class FastTableData : ITableData
+{
+    private readonly List<FastRow> _rows;
+    public int RowCount => _rows.Count;
+    public IEnumerable<string> Columns { get; }
+
+    public FastTableData(IEnumerable<string> columns, List<FastRow> rows)
+    {
+        _rows = rows;
+        Columns = columns;
+    }
+
+    public object GetValue(string columnName, int rowIndex)
+    {
+        return _rows[rowIndex].GetAtColumn(columnName);
     }
 }
--- DotNetUnderTheHoodAssignment/Program.cs
+++ DotNetUnderTheHoodAssignment/Program.cs
@@ -27,33 +27,33 @@ Console.WriteLine($"Time of reading the CSV was " +
 ITableDataBuilder fastTableDataBuiler = new FastTableDataBuilder();
 
 //TODO uncomment when new code is ready
-//var testResultForNewCode = TableDataPerformanceMeasurer.Test(
-//    fastTableDataBuiler, csvData);
+var testResultForNewCode = TableDataPerformanceMeasurer.Test(
+    fastTableDataBuiler, csvData);
 
-//Console.WriteLine();
-//Console.WriteLine("Test results for new code:");
-//Console.WriteLine("Memory increase in bytes: " +
-//    string.Format("{0:n0}", testResultForNewCode.MemoryIncreaseInBytes));
-//Console.WriteLine($"Time of loading the CSV was " +
-//    $"{testResultForNewCode.TimeOfBuildingTable}.");
-//Console.WriteLine($"Time of reading the CSV was " +
-//    $"{testResultForNewCode.TimeOfDataReading}.");
+Console.WriteLine();
+Console.WriteLine("Test results for new code:");
+Console.WriteLine("Memory increase in bytes: " +
+    string.Format("{0:n0}", testResultForNewCode.MemoryIncreaseInBytes));
+Console.WriteLine($"Time of loading the CSV was " +
+    $"{testResultForNewCode.TimeOfBuildingTable}.");
+Console.WriteLine($"Time of reading the CSV was " +
+    $"{testResultForNewCode.TimeOfDataReading}.");
 
-//Console.WriteLine();
-//Console.WriteLine("Checking if results are the same...");
-//var areEqual = ContentEqualityChecker.IsEqual(
-//    tableDataBuiler, 
-//    fastTableDataBuiler, 
-//    csvData);
+Console.WriteLine();
+Console.WriteLine("Checking if results are the same...");
+var areEqual = ContentEqualityChecker.IsEqual(
+    tableDataBuiler,
+    fastTableDataBuiler,
+    csvData);
 
-//if(areEqual)
-//{
-//    Console.WriteLine("Results are the same.");
-//}
-//else
-//{
-//    Console.WriteLine("Results are different.");
-//}
+if (areEqual)
+{
+    Console.WriteLine("Results are the same.");
+}
+else
+{
+    Console.WriteLine("Results are different.");
+}
 
 Console.WriteLine("Done. Press any key to close.");
 Console.ReadKey();
