6cb35caa428122f7cb477c7318c740e82246b1b9
DanielMevs <dannymevs@hotmail.com>
2024-05-27 15:43:04 -0400

    improved csv processing by reducing the number of boxings

--- DotNetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
+++ DotNetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
@@ -12,86 +12,43 @@ public class FastTableDataBuilder : ITableDataBuilder
 
         foreach (var row in csvData.Rows)
         {
-            var newRowData = new Dictionary<string, object>();
-
+            var newRow = new FastRow();
             for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
             {
                 var column = csvData.Columns[columnIndex];
                 string valueAsString = row[columnIndex];
-                object value = ConvertValueToTargetType(valueAsString);
-
-                if (value is not null)
+                if (string.IsNullOrEmpty(valueAsString))
                 {
-                    newRowData[column] = value;
+                    continue;
+                }
+                else if (valueAsString == "TRUE")
+                {
+                    newRow.AssignCell(column, true);
+                }
+                else if (valueAsString == "FALSE")
+                {
+                    newRow.AssignCell(column, false);
+                }
+                else if (valueAsString.Contains(".")
+                    && decimal.TryParse(valueAsString, out var valueAsDecimal))
+                {
+                    newRow.AssignCell(column, valueAsDecimal);
+                }
+                else if (int.TryParse(valueAsString, out var valueAsInt))
+                {
+                    newRow.AssignCell(column, valueAsInt);
+                }
+                else
+                {
+                    newRow.AssignCell(column, valueAsString);
                 }
             }
 
-            resultRows.Add(new FastRow(newRowData));
+            resultRows.Add(newRow);
         }
 
         return new FastTableData(csvData.Columns, resultRows);
     }
 
-    private object ConvertValueToTargetType(string value)
-    {
-        if (string.IsNullOrEmpty(value))
-        {
-            return null;
-        }
-        if (value == "TRUE")
-        {
-            return true;
-        }
-        if (value == "FALSE")
-        {
-            return false;
-        }
-        if (value.Contains(".") && decimal.TryParse(value, out var valueAsDecimal))
-        {
-            return valueAsDecimal;
-        }
-        if (int.TryParse(value, out var valueAsInt))
-        {
-            return valueAsInt;
-        }
-        return value;
-    }
-}
-
-public class FastRow
-{
-    private Dictionary<string, object> _data;
-
-    public FastRow(Dictionary<string, object> data)
-    {
-        _data = data;
-    }
-
-    public object GetAtColumn(string columnName)
-    {
-        if (_data.ContainsKey(columnName))
-        {
-            return _data[columnName];
-        }
-
-        return null;
-    }
-}
-
-public class FastTableData : ITableData
-{
-    private readonly List<FastRow> _rows;
-    public int RowCount => _rows.Count;
-    public IEnumerable<string> Columns { get; }
-
-    public FastTableData(IEnumerable<string> columns, List<FastRow> rows)
-    {
-        _rows = rows;
-        Columns = columns;
-    }
-
-    public object GetValue(string columnName, int rowIndex)
-    {
-        return _rows[rowIndex].GetAtColumn(columnName);
-    }
+    
 }
