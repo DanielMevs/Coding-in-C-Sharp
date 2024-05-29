f9cade09221a8227cbb08ecb3b26f9aa75793381
DanielMevs <dannymevs@hotmail.com>
2024-05-28 20:02:38 -0400

    completed reflection section

---
+++ AdvancedC#Types/Program.cs
@@ -0,0 +1,25 @@
+﻿var converter = new ObjectToTextConverter();
+Console.WriteLine(converter.Convert(
+    new House("123 Maple Rd", 179.6, 2)));
+
+Console.ReadKey();
+class ObjectToTextConverter
+{
+    public string Convert(object obj)
+    {
+        Type type = obj.GetType();
+        var properties = type
+            .GetProperties()
+            .Where(p => p.Name != "EqualityContract");
+
+        return String.Join(
+            ", ",
+            properties
+            .Select(property =>
+            $"{property.Name} is {property.GetValue(obj)}"));
+    }
+}
+
+public record Pet(string Name, PetType PetType, float Weight);
+public record House(string Address, double Area, int Floors);
+public enum PetType {  Cat, Dog, Fish };
