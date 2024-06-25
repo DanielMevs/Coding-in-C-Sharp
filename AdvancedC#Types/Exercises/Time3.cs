a0b68d41e456b9a21a1ea436a1822eb616250e19
DanielMevs <dannymevs@hotmail.com>
2024-06-24 20:03:47 -0400

    completed ValueTuples section and overriding Equals and GetHashCode in Time struct exercise

---
+++ AdvancedC#Types/Exercises/Time3.cs
@@ -0,0 +1,37 @@
+﻿namespace Coding.Exercise
+{
+    public struct Time
+    {
+        public int Hour { get; }
+        public int Minute { get; }
+
+        public Time(int hour, int minute)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "Hour is out of range of 0-23");
+            }
+            if (minute < 0 || minute > 59)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "Minute is out of range of 0-59");
+            }
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public override string ToString() =>
+            $"{Hour.ToString("00")}:{Minute.ToString("00")}";
+
+        //your code goes here
+        public bool Equals(Time other)
+        {
+            return Hour == other.Hour && Minute == other.Minute;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hour, Minute);
+        }
+    }
+}
--- AdvancedC#Types/Program.cs
+++ AdvancedC#Types/Program.cs
@@ -145,12 +145,30 @@ var dictionary = new Dictionary<Point, int>();
 //Console.WriteLine(point2.GetHashCode());
 //Console.WriteLine(point3.GetHashCode());
 
-var person1 = new Person(6, "Martin");
-var person2 = new Person(6, "Martin");
-var person3 = new Person(7, "Bella");
-Console.WriteLine(person1.GetHashCode());
-Console.WriteLine(person2.GetHashCode());
-Console.WriteLine(person3.GetHashCode());
+//var person1 = new Person(6, "Martin");
+//var person2 = new Person(6, "Martin");
+//var person3 = new Person(7, "Bella");
+//Console.WriteLine(person1.GetHashCode());
+//Console.WriteLine(person2.GetHashCode());
+//Console.WriteLine(person3.GetHashCode());
+
+var tuple1 = new Tuple<string, bool>("aaa", false);
+var tuple2 = Tuple.Create(10, true);
+var tuple3 = Tuple.Create(10, true);
+Console.WriteLine(tuple2 == tuple3);
+Console.WriteLine(tuple2.Equals(tuple3));
+Console.WriteLine(tuple2.GetHashCode());
+Console.WriteLine(tuple3.GetHashCode());
+
+var number = tuple2.Item1;
+// Tuples are Readonly
+//tuple2.Item1 = 20;
+
+var valueTuple1 = new ValueTuple<int, string>(1, "bbb");
+var valueTuple2 = (Number: 5, Text: "ccc");
+valueTuple2.Item1 = 20;
+valueTuple2.Text = "ddd";
+
 
 Console.ReadKey();
 
