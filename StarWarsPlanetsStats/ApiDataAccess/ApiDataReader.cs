da034d97a5e408d4492872adf21623f9c22ea79c
DanielMevs <dannymevs@hotmail.com>
2024-07-04 20:42:21 -0400

    started implementing star wars planets stats

---
+++ StarWarsPlanetsStats/ApiDataAccess/ApiDataReader.cs
@@ -0,0 +1,16 @@
+﻿namespace StarWarsPlanetsStats.ApiDataAccess;
+
+public class ApiDataReader : IApiDataReader
+{
+    public async Task<string> Read(
+        string baseAddress, string requestUri)
+    {
+        using var client = new HttpClient();
+        client.BaseAddress = new Uri(baseAddress);
+        HttpResponseMessage response = await client.GetAsync(
+            requestUri);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
---
+++ StarWarsPlanetsStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using StarWarsPlanetsStats.ApiDataAccess;
+using StarWarsPlanetsStats.DTOs;
+using System.Text.Json;
+
+IApiDataReader apiDataReader = new ApiDataReader();
+var json = await apiDataReader.Read(
+    "https://swapi.dev", "api/planets");
+
+var root = JsonSerializer.Deserialize<Root>(json);
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
