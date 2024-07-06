65fff0a34e2c181698413532c49ddc048f8c8f76
DanielMevs <dannymevs@hotmail.com>
2024-07-06 07:45:51 -0400

    worked on type design for star wars planets stats app

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -32,7 +32,7 @@ public class StarWarsPlanetsStatsApp
     
     public async Task Run()
     {
-        string json = null;
+        string? json = null;
         try
         {
             json = await _apiDataReader.Read(
@@ -52,5 +52,37 @@ public class StarWarsPlanetsStatsApp
         }
 
         var root = JsonSerializer.Deserialize<Root>(json);
+        var planets = ToPlanets(root);
+    }
+
+    private IEnumerable<Planet> ToPlanets(Root? root)
+    {
+        if (root is null)
+        {
+            throw new NotImplementedException();
+
+        }
+        throw new NotImplementedException();
+    }
+}
+
+public readonly record struct Planet
+{
+    public string Name { get; }
+    public int Diameter { get; }
+    public int? SurfaceWater { get; }
+    public int? Population { get; }
+
+    public Planet(
+        string name,
+        int diameter,
+        int? surfaceWater,
+        int? population)
+    {
+        if (name is null)
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+        Name = name;
     }
 }
