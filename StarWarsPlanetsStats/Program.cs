2c83257b834d9e5547b4237f220c9387c2330d98
DanielMevs <dannymevs@hotmail.com>
2024-07-05 20:35:08 -0400

    implemented exception handling to star wars planets stats app

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -2,11 +2,55 @@
 using StarWarsPlanetsStats.DTOs;
 using System.Text.Json;
 
-IApiDataReader apiDataReader = new ApiDataReader();
-var json = await apiDataReader.Read(
-    "https://swapi.dev", "api/planets");
-
-var root = JsonSerializer.Deserialize<Root>(json);
-
+try
+{
+    await new StarWarsPlanetsStatsApp(
+        new ApiDataReader(),
+        new MockStarWarsApiDataReader()).Run();
+}
+catch(Exception ex)
+{
+    Console.WriteLine("An error occured. " +
+        "Exception message: " + ex.Message);
+}
 Console.WriteLine("Press any key to close.");
 Console.ReadKey();
+
+public class StarWarsPlanetsStatsApp
+{
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondaryApiDataReader;
+
+    public StarWarsPlanetsStatsApp(
+        IApiDataReader apiDataReader,
+        IApiDataReader secondaryApiDataReader)
+    {
+        _apiDataReader = apiDataReader;
+        _secondaryApiDataReader = secondaryApiDataReader;
+    }
+
+    
+    public async Task Run()
+    {
+        string json = null;
+        try
+        {
+            json = await _apiDataReader.Read(
+                "https://swapi.dev", "api/planets");
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("API request was unsuccessful. " +
+                "Switch to mock data. " +
+                "Exception message: " + ex.Message);
+        }
+
+        if (json is null)
+        {
+            json = await _secondaryApiDataReader.Read(
+                "https://swapi.dev", "api/planets");
+        }
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+    }
+}
