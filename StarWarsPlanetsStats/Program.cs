6a2a1cf268d7c989153f63d33935c80dc4452381
DanielMevs <dannymevs@hotmail.com>
2024-07-08 20:25:07 -0400

    added finishing touches to Star Wars planets statistics app

--- StarWarsPlanetsStats/Program.cs
+++ StarWarsPlanetsStats/Program.cs
@@ -53,6 +53,63 @@ public class StarWarsPlanetsStatsApp
 
         var root = JsonSerializer.Deserialize<Root>(json);
         var planets = ToPlanets(root);
+
+        foreach(var planet in planets)
+        {
+            Console.WriteLine(planet);
+        }
+        Console.WriteLine();
+        Console.WriteLine("The statistics of which property " +
+            "would you like to see?");
+        Console.WriteLine("population");
+        Console.WriteLine("diameter");
+        Console.WriteLine("surface water");
+
+        var userChoice = Console.ReadLine();
+
+        if(userChoice == "population")
+        {
+            ShowStatistics(
+                planets,
+                "population",
+                planet => planet.Population);
+        }
+        else if(userChoice == "diameter")
+        {
+            ShowStatistics(
+                planets,
+                "diameter",
+                planet => planet.Diameter);
+        }
+        else if (userChoice == "surface water")
+        {
+            ShowStatistics(
+                planets,
+                "surface water",
+                planet => planet.SurfaceWater);
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice!");
+        }
+    }
+    private void ShowStatistics(IEnumerable<Planet> planets,
+        string propertyName,
+        Func<Planet, int?> propertySelector)
+    {
+        var planetByMaxPropertyValue = planets.MaxBy(propertySelector);
+
+        Console.WriteLine("Planet with the max " +
+            $"{propertyName} is " +
+            $"{propertySelector(planetByMaxPropertyValue)} " +
+            $"({planetByMaxPropertyValue.Name})");
+
+        var planetByMinPropertyValue = planets.MinBy(propertySelector);
+
+        Console.WriteLine("Planet with the min " +
+            $"{propertyName} is " +
+            $"{propertySelector(planetByMinPropertyValue)} " +
+            $"({planetByMinPropertyValue.Name})");
     }
 
     private IEnumerable<Planet> ToPlanets(Root? root)
