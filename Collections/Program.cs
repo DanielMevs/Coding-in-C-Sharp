d1f28fc6c5fdf9a4f6aeb55f683f16aab14056d4
DanielMevs <dannymevs@hotmail.com>
2024-08-26 19:41:52 -0400

    explored the IEnumerable interface

---
+++ Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+
+var text = "hello there";
+foreach(char character in text)
+{
+    Console.WriteLine(character);
+}
+
+//var customCollection = new CustomCollection(
+//    new string[] { "aaa", "bbb", "ccc" });
+
+//foreach(var word in customCollection)
+//{
+//    Console.WriteLine(word);
+//}
+var words = new string[] { "aaa", "bbb", "ccc" };
+//foreach(var word in words)
+//{
+//    Console.WriteLine(word);
+//}
+
+IEnumerator wordsEnumerator = words.GetEnumerator();
+object currentWord;
+while (wordsEnumerator.MoveNext())
+{
+    currentWord = wordsEnumerator.Current;
+    Console.WriteLine(currentWord);
+}
+
+Console.ReadKey();
+
+public class CustomCollection //: IEnumerable<string>
+{
+    public string[] Words { get; }
+    public CustomCollection(string[] words)
+    {
+        Words = words;
+    }
+}
