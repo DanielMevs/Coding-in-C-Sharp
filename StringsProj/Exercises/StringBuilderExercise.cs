5226f9bfafcda43d82beaff4b40702955a3000a3
DanielMevs <dannymevs@hotmail.com>
2024-09-21 16:36:25 -0400

    completed StringBuilder exercise and examined string interning and the flyweight design pattern

---
+++ StringsProj/Exercises/StringBuilderExercise.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace StringsProj.Exercises
+{
+    public class StringBuilderExercise
+    {
+        public static string Reverse(string input)
+        {
+            var stringBuilder = new StringBuilder();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                stringBuilder.Append(input[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- StringsProj/Program.cs
+++ StringsProj/Program.cs
@@ -48,44 +48,86 @@ using System.Text;
 //Modify(text);
 //Console.WriteLine(text1);
 
-var test = new TestStruct
-{
-    Text = "abc"
-};
+//var test = new TestStruct
+//{
+//    Text = "abc"
+//};
 
-var other = test;
+//var other = test;
 
 //var other = new TestStruct
 //{
 //    List = test.List
 //};
 
-const int Count = 200_000;
+//const int Count = 200_000;
 
-var text = string.Empty;
-Stopwatch stopwatch = Stopwatch.StartNew();
-for(int i = 0; i < Count; i++)
-{
-    text += "a";
-}
-stopwatch.Stop();
-Console.WriteLine(
-    $"Concatenation took {stopwatch.ElapsedMilliseconds} ms");
+//var text = string.Empty;
+//Stopwatch stopwatch = Stopwatch.StartNew();
+//for(int i = 0; i < Count; i++)
+//{
+//    text += "a";
+//}
+//stopwatch.Stop();
+//Console.WriteLine(
+//    $"Concatenation took {stopwatch.ElapsedMilliseconds} ms");
 
-stopwatch.Restart();
-var stringBuilder = new StringBuilder();
+//stopwatch.Restart();
+//var stringBuilder = new StringBuilder();
 
-for(int i = 0;i < Count; i++)
-{
-    stringBuilder.Append("a");
-}
-var text2 = stringBuilder.ToString();
-stopwatch.Stop();
-Console.WriteLine(
-    $"Concatenation took {stopwatch.ElapsedMilliseconds} ms");
+//for(int i = 0;i < Count; i++)
+//{
+//    stringBuilder.Append("a");
+//}
+//var text2 = stringBuilder.ToString();
+//stopwatch.Stop();
+//Console.WriteLine(
+//    $"Concatenation took {stopwatch.ElapsedMilliseconds} ms");
+
+const int Count = 1000;
+TestCharArraysMemoryConsumption(Count);
+TestStringsMemoryConsumption(Count);
+
+string text1 = "abc";
+string text2 = "abc";
+
+Console.WriteLine(object.ReferenceEquals(text1, text2));
 
 
 Console.ReadKey();
+
+void TestCharArraysMemoryConsumption(int count)
+{
+    var list = new List<char[] >(count);
+    GC.Collect(2, GCCollectionMode.Default, true);
+    var memoryBefore = GC.GetTotalMemory(false);
+    for (int i = 0; i < count; ++i)
+    {
+        list.Add(new char[] {'a', 'a', 'a', 'a', 'a', 'a' });
+    }
+    GC.Collect(2, GCCollectionMode.Default, true);
+    var memoryAfter = GC.GetTotalMemory(false);
+    Console.WriteLine(
+        "difference in bytes is " + (memoryAfter - memoryBefore));
+
+}
+
+void TestStringsMemoryConsumption(int count)
+{
+    var list = new List<string>(count);
+    GC.Collect(2, GCCollectionMode.Default, true);
+    var memoryBefore = GC.GetTotalMemory(false);
+    for (int i = 0; i < count; ++i)
+    {
+        //list.Add($"aaaaa{i}");
+        list.Add("aaaaa");
+    }
+    GC.Collect(2, GCCollectionMode.Default, true);
+    var memoryAfter = GC.GetTotalMemory(false);
+    Console.WriteLine(
+        "difference in bytes is " + (memoryAfter - memoryBefore));
+
+}
 
 
 void Modify(string input)
