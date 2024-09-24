99763377405345a37c5acbe510b2d7287d40f3f4
DanielMevs <dannymevs@hotmail.com>
2024-09-23 20:07:11 -0400

    started tickets data aggregator assignment

--- StringsProj/Program.cs
+++ StringsProj/Program.cs
@@ -93,45 +93,54 @@ using System.Globalization;
 
 //Console.WriteLine(object.ReferenceEquals(text1, text2));
 
-var number1 = 100;
-var number2 = 200;
+//var number1 = 100;
+//var number2 = 200;
 
-var text = string.Format("Number 1 {0}, number 2 is {1,10:C}",
-    number1, number2);
+//var text = string.Format("Number 1 {0}, number 2 is {1,10:C}",
+//    number1, number2);
 
-Console.WriteLine(text);
+//Console.WriteLine(text);
 
-decimal someDecimal = 1.46m;
-Console.WriteLine(
-    String.Format("Number is {0:C3}", someDecimal));
-Console.WriteLine(
-    String.Format("Number is {0:F1}", someDecimal));
-Console.WriteLine(
-    String.Format("Number is {0:P}", someDecimal));
+//decimal someDecimal = 1.46m;
+//Console.WriteLine(
+//    String.Format("Number is {0:C3}", someDecimal));
+//Console.WriteLine(
+//    String.Format("Number is {0:F1}", someDecimal));
+//Console.WriteLine(
+//    String.Format("Number is {0:P}", someDecimal));
 
-DateTime someDate = new DateTime(2024, 5, 6, 12, 54, 12);
-Console.WriteLine(
-    $"Date is {someDate:d}");
-Console.WriteLine(
-    $"Date is {someDate:D}");
-Console.WriteLine(
-    String.Format("Date is {0:MM/yyyy}", someDate));
+//DateTime someDate = new DateTime(2024, 5, 6, 12, 54, 12);
+//Console.WriteLine(
+//    $"Date is {someDate:d}");
+//Console.WriteLine(
+//    $"Date is {someDate:D}");
+//Console.WriteLine(
+//    String.Format("Date is {0:MM/yyyy}", someDate));
 
-CultureInfo currentCulture = CultureInfo.CurrentCulture;
-Console.WriteLine("en-US culture");
-Console.WriteLine(currentCulture);
+//CultureInfo currentCulture = CultureInfo.CurrentCulture;
+//Console.WriteLine("en-US culture");
+//Console.WriteLine(currentCulture);
+
+//var date = new DateTime(2025, 3, 2, 12, 16, 14);
+//var number = 1.9m;
+
+//Console.WriteLine(date.ToString("d"));
+//Console.WriteLine(number);
+
+//Console.WriteLine("pl-PL culture");
+//CultureInfo.CurrentCulture = new CultureInfo("pl-PL");
+//Console.WriteLine(date.ToString("d"));
+//Console.WriteLine(number);
+
+//Console.WriteLine("Enter a number");
+//var userInput = Console.ReadLine();
+//decimal result = decimal.Parse(userInput);
+//Console.WriteLine($"Number is {result}");
 
 var date = new DateTime(2025, 3, 2, 12, 16, 14);
-var number = 1.9m;
 
-Console.WriteLine(date.ToString("d"));
-Console.WriteLine(number);
-
-Console.WriteLine("pl-PL culture");
-CultureInfo.CurrentCulture = new CultureInfo("pl-PL");
-Console.WriteLine(date.ToString("d"));
-Console.WriteLine(number);
-
+Console.WriteLine(
+    date.ToString("d", CultureInfo.InvariantCulture));
 
 Console.ReadKey();
 
---
+++ TicketsDataAggregator/Program.cs
@@ -0,0 +1,55 @@
+﻿// Get the current working directory
+using System.Reflection;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+
+string assemblyPath = Assembly.GetExecutingAssembly().Location;
+string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+string repositoryRoot = Directory.GetParent(assemblyDirectory).Parent.Parent.FullName;
+string TicketsFolder = Path.Combine(repositoryRoot, "Tickets");
+
+// Output the path
+Console.WriteLine(TicketsFolder);
+
+
+try
+{
+    var ticketsAggregator = new TicketsAggregator(
+        TicketsFolder);
+    ticketsAggregator.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An exception occurred. " +
+        "Exception message: " + ex.Message);
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+public class TicketsAggregator
+{
+    private readonly string _ticketsFolder;
+
+    public TicketsAggregator(string ticketsFolder)
+    {
+        _ticketsFolder = ticketsFolder;
+    }
+
+    public void Run()
+    {
+        foreach (var filePath in Directory.GetFiles(
+            _ticketsFolder, "*.pdf"))
+        {
+            using (PdfDocument document = PdfDocument.Open(filePath))
+            {
+                // Page number starts from 1, not 0.
+                Page page = document.GetPage(1);
+                string text = page.Text;
+            }
+        }
+        
+    }
+}
+
