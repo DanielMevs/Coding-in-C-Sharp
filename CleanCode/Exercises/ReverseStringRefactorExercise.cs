63805e8af4a1ede778e7cee6b0d34c9fece0de2e
DanielMevs <dannymevs@hotmail.com>
2024-10-11 19:22:48 -0400

    completed reverse string refactoring exercise

---
+++ CleanCode/Exercises/ReverseStringRefactorExercise.cs
@@ -0,0 +1,36 @@
+﻿namespace CleanCode.Exercises
+{
+    public class ReverseStringRefactorExercise
+    {
+        public static string Reverse_Refactored(string input)
+        {
+            var resultCharacters = new char[input.Length];
+            int currentIndex = input.Length - 1;
+
+            foreach (var character in input)
+            {
+                resultCharacters[currentIndex] = character;
+                --currentIndex;
+            }
+
+            return new string(resultCharacters);
+
+        }
+
+        //do not modify this method!
+        public static string Reverse(string str)
+        {
+            var arr = new char[str.Length];
+
+            var i = str.Length - 1;
+            foreach (var _char in str)
+            {
+                arr[i] = _char;
+                --i;
+            }
+
+            return new string(arr);
+        }
+    }
+}
+
