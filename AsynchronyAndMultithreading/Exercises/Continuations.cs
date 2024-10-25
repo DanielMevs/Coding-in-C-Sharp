66ce3f8dba6ab62f723ae18ac0d908207d3a18e0
DanielMevs <dannymevs@hotmail.com>
2024-10-25 19:38:33 -0400

    Explored the concepts of race conditions and locks, and experimented with async await

---
+++ AsynchronyAndMultithreading/Exercises/Continuations.cs
@@ -0,0 +1,57 @@
+﻿namespace AsynchronyAndMultithreading.Exercises
+{
+    public class Continuations
+    {
+        public static Task Test(string? input)
+        {
+            var task = Task.Run(() => ParseToIntAndPrint(input))
+                .ContinueWith(faultedTask =>
+                {
+                    if (faultedTask.IsFaulted && faultedTask.Exception != null)
+                    {
+                        faultedTask.Exception.Handle(ex =>
+                        {
+                            if (ex is ArgumentNullException)
+                            {
+                                Console.WriteLine("The input is null.");
+                                return true; // Exception handled
+                            }
+                            if (ex is FormatException)
+                            {
+                                Console.WriteLine("The input is not in a correct format.");
+                                return true; // Exception handled
+                            }
+                            if (ex is ArgumentOutOfRangeException)
+                            {
+                                Console.WriteLine("Unexpected exception type.");
+                                return false; // Exception not handled, task remains faulted
+                            }
+                            return false; // Any other exception, task remains faulted
+                        });
+                    }
+                }, TaskContinuationOptions.OnlyOnFaulted);
+
+            return task;
+        }
+
+        private static void ParseToIntAndPrint(string? input)
+        {
+            if (input is null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (long.TryParse(input, out long result))
+            {
+                if (result > int.MaxValue)
+                {
+                    throw new ArgumentOutOfRangeException("The number is too big for an int.");
+                }
+                Console.WriteLine("Parsing successful, the result is: " + result);
+            }
+            else
+            {
+                throw new FormatException();
+            }
+        }
+    }
+}
--- AsynchronyAndMultithreading/Program.cs
+++ AsynchronyAndMultithreading/Program.cs
@@ -66,10 +66,52 @@
 //    Console.WriteLine("Enter a command:");
 //    userInput = Console.ReadLine();
 //} while (userInput != "exit");
+
+
+//var task = Task.Run(() => Divide(2, 0))
+//    .ContinueWith(
+//    faultedTask =>
+//    {
+//        faultedTask.Exception.Handle(ex =>
+//        {
+//            Console.WriteLine("Division task finsihed");
+//            if (ex is ArgumentNullException)
+//            {
+//                Console.WriteLine("Arguments can't be null");
+//                return true;
+//            }
+//            if (ex is DivideByZeroException)
+//            {
+//                Console.WriteLine("Can't divide by zero.");
+//                return true;
+//            }
+//            Console.WriteLine("Unexpected exception type.");
+//            return false;
+//        });
+//    },
+//    TaskContinuationOptions.OnlyOnFaulted);
+////////////////////////////////////////////
+//var task = new Task(() => Divide(10, 2));
+
+//task.ContinueWith(faultedTask =>
+//    Console.WriteLine("Success"),
+//    TaskContinuationOptions.OnlyOnRanToCompletion);
+
+//task.ContinueWith(faultedTask =>
+//    Console.WriteLine("Exception thrown: " + faultedTask.Exception.Message),
+//    TaskContinuationOptions.OnlyOnFaulted);
+
+//task.Start();
+//////////////////////////////////////////////////////////
+//Console.WriteLine("Main thread's ID: " + Thread.CurrentThread.ManagedThreadId);
+
 //var cancellationTokenSource = new CancellationTokenSource();
 //var task = Task.Run(
 //    () => NeverendingMethod(cancellationTokenSource.Token),
-//    cancellationTokenSource.Token);
+//    cancellationTokenSource.Token)
+//    .ContinueWith(canceledTask =>
+//        Console.WriteLine($"Task with ID {canceledTask.Id} has been canceled."),
+//        TaskContinuationOptions.OnlyOnCanceled);
 
 //string userInput;
 //do
@@ -79,39 +121,62 @@
 
 //cancellationTokenSource.Cancel();
 
+//Thread.Sleep(2000);
+//Console.WriteLine("Program is finished.");
+///////////////////////////////////////////
+//var counter = new Counter();
+//var tasks = new List<Task>();
 
-Console.WriteLine("Main thread's ID: " + Thread.CurrentThread.ManagedThreadId);
+//for(int i = 0; i < 10; i++)
+//{
+//    tasks.Add(Task.Run(() => counter.Increment()));
+//}
+//for(int i = 0;i < 10; i++)
+//{
+//    tasks.Add(Task.Run(() => counter.Decrement()));
+//}
 
+//Task.WaitAll(tasks.ToArray());
+//Console.WriteLine("Counter value is: " + counter.Value);
 
-var task = Task.Run(() => Divide(2, 0))
-    .ContinueWith(
-    faultedTask =>
-    {
-        faultedTask.Exception.Handle(ex =>
-        {
-            Console.WriteLine("Division task finsihed");
-            if (ex is ArgumentNullException)
-            {
-                Console.WriteLine("Arguments can't be null");
-                return true;
-            }
-            if (ex is DivideByZeroException)
-            {
-                Console.WriteLine("Can't divide by zero.");
-                return true;
-            }
-            Console.WriteLine("Unexpected exception type.");
-            return false;
-        });
-    },
-    TaskContinuationOptions.OnlyOnFaulted);
-        
+// synchronous
+//var result = CalculateLength("Hello");
+//Print(result);
+//Console.WriteLine("The process is finished.");
+////////////////////////////////////////////////////
+//var task = Task.Run(() => CalculateLength("Hello"))
+//    .ContinueWith(completedTask => Print(completedTask.Result))
+//    .ContinueWith(previousContinuation =>
+//        Console.WriteLine("The process is finished."));
 
+//string userInput;
+//do
+//{
+//    userInput = Console.ReadLine();
+//} while (userInput != "stop");
 
+//Console.WriteLine("Program is finished.");
+////////////////////////////////////////////////
 
-Thread.Sleep(1000);
-Console.WriteLine("Program is finished.");
+var task = RunHeavyProcess();
+
+Console.WriteLine("Doing other work");
+Console.WriteLine("Doing even more work");
+
 Console.ReadKey();
+
+static async Task RunHeavyProcess()
+{
+    string result = await HeavyCalculation();
+    Console.WriteLine(result);
+}
+
+static async Task<string> HeavyCalculation()
+{
+    Console.WriteLine("Starting heavy calculation");
+    Thread.Sleep(2000);
+    return "Done!";
+}
 
 static float Divide(int? a, int? b)
 {
@@ -145,9 +210,17 @@ static void NeverendingMethod(CancellationToken cancellationToken)
 
 static int CalculateLength(string input)
 {
-    Console.WriteLine("CalculateLength thread's ID: " + Thread.CurrentThread.ManagedThreadId);
+    //Console.WriteLine("CalculateLength thread's ID: " + Thread.CurrentThread.ManagedThreadId);
+    Console.WriteLine("Starting the CalculateLength method");
     Thread.Sleep(4000);
     return input.Length;
+}
+
+static void Print(int result)
+{
+    Console.WriteLine("Starting the Print method");
+    Thread.Sleep(2000);
+    Console.WriteLine($"The result is {result}");
 }
 
 static void PrintA(object obj)
@@ -169,5 +242,25 @@ static void PrintMinuses(int n)
     for (int i = 0; i < n; i++)
     {
         Console.Write("-");
+    }
+}
+
+class Counter
+{
+    private object _valueLock = new object();
+    public int Value { get; private set; }
+    public void Increment()
+    {
+        lock (_valueLock)
+        {
+            Value++;
+        }
+    }
+    public void Decrement()
+    {
+        lock (_valueLock)
+        {
+            Value--;
+        }
     }
 }
