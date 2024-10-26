9662f4c32d7b6c26f20e057400bfa1603a41e507
DanielMevs <dannymevs@hotmail.com>
2024-10-26 15:12:56 -0400

    Examined the flow of an asynchronous program.

--- AsynchronyAndMultithreading/Program.cs
+++ AsynchronyAndMultithreading/Program.cs
@@ -157,7 +157,7 @@
 
 //Console.WriteLine("Program is finished.");
 ////////////////////////////////////////////////
-
+Console.WriteLine("Main thread's ID: " + Thread.CurrentThread.ManagedThreadId);
 var task = RunHeavyProcess();
 
 Console.WriteLine("Doing other work");
@@ -167,14 +167,16 @@ Console.ReadKey();
 
 static async Task RunHeavyProcess()
 {
+    Console.WriteLine("RunHeavyProcess thread's ID: " + Thread.CurrentThread.ManagedThreadId);
     string result = await HeavyCalculation();
     Console.WriteLine(result);
 }
 
 static async Task<string> HeavyCalculation()
 {
+    Console.WriteLine("HeavyCalculation thread's ID: " + Thread.CurrentThread.ManagedThreadId);
     Console.WriteLine("Starting heavy calculation");
-    Thread.Sleep(2000);
+    await Task.Delay(2000);
     return "Done!";
 }
 
