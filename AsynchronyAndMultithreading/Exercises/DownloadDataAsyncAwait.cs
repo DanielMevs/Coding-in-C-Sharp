3d033c2f0bbac880808c9b5d86b69fdec06fe120
DanielMevs <dannymevs@hotmail.com>
2024-10-26 20:14:07 -0400

    Completed async/await exercise download data async

---
+++ AsynchronyAndMultithreading/Exercises/DownloadDataAsyncAwait.cs
@@ -0,0 +1,19 @@
+﻿namespace AsynchronyAndMultithreading.Exercises
+{
+    public class DownloadDataAsyncAwait
+    {
+        public async void Test()
+        {
+            string data = await DownloadDataAsync("test.com", "some content");
+            Console.WriteLine(data);
+        }
+
+        //your code goes here
+        public async Task<string> DownloadDataAsync(string url, string content)
+        {
+            await Task.Delay(1000);
+            return $"Content from URL '{url}' is '{content}'";
+
+        }
+    }
+}
